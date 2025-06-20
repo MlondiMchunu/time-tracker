deb26362d37ccf953d1fa8464a8a9ca5e0c9b375
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-06-20 18:41:13 +0200

    complete form design and add horizontal line under Time Tracker

--- time-tracker/src/App.tsx
+++ time-tracker/src/App.tsx
@@ -8,20 +8,29 @@ function App() {
   const [taskName, setTaskName] = useState('');
   const [hoursWorked, setHoursWorked] = useState<number>(0);
 
-const handleSubmit = (e:React.FormEvent)=>{
-  e.preventDefault();
-}
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  }
 
   return (
     <>
       <div className="">
-        <h2>Time Tracker</h2>
+        <h2>Time Tracker</h2> <hr></hr>
       </div>
 
-<div>
-  <h4>Add New Entry</h4>
-  <form onSubmit={handleSubmit}></form>
-</div>
+      <div>
+        <h4>Add New Entry</h4>
+        <form onSubmit={handleSubmit}></form>
+        <div>
+          <label htmlFor='taskName'>Task Name : </label>
+          <input type="text" id="taskName" value={taskName} onChange={(e)=>setTaskName(e.target.value)} required></input>
+        </div>
+        <br></br>
+        <div>
+          <label htmlFor='hoursWorked'>Hours Worked : </label>
+          <input type='number' id='hoursWorked' value={hoursWorked || ''} onChange={(e)=>setHoursWorked(Number(e.target.value))} min="0" step="0.25" placeholder="0" required></input>
+        </div>
+      </div>
     </>
   )
 }
